91315a16c37315b81c4e83a3e0ef41bac6e80610
sgmt <s.shigematsu17@gmail.com>
2026-03-10 10:56:26 +0900

    feat: 記事取得、作成、更新、削除機能を実装 fix: docker-composeのPostgresポートを5433に変更

--- backend/src/routes/articles/articles.router.js
+++ backend/src/routes/articles/articles.router.js
@@ -1,20 +1,88 @@
-import articleService from '../../services/articles/ArticleService.js';
-import express from 'express';
-import authenticate from '../../middleware/authenticate.js';
+import express from "express";
+import ArticleService from "../../services/articles/ArticleService.js";
 
 const router = express.Router();
+const articleService = new ArticleService();
 
-/**
- * メモ新規登録
- */
-router.get('/createArticle', authenticate, async (req, res, next) => {
+// 記事一覧取得
+router.get("/", async (req, res) => {
   try {
-    let body = {};
-
-    res.status(200).json(body);
+    // const user_id = req.user.id;
+    const user_id = 1; // 仮のユーザーID
+    const articleList = await articleService.getArticleList(user_id);
+    res.status(200).json(articleList);
   } catch (error) {
     console.error(error);
     res.status(500).json({});
+  }
+});
+
+// 記事情報取得
+router.get("/:id", async (req, res) => {
+  try {
+    // const user_id = req.user.user_id;
+    const user_id = 1; // 仮のユーザーID
+    const article_id = req.params.id;
+    const article = await articleService.getArticle(user_id, article_id);
+
+    res.json(article);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+// メモ新規登録
+router.post("/", async (req, res) => {
+  try {
+    // const user_id = req.user.user_id;
+    const user_id = 1; // 仮のユーザーID
+    const { title, content } = req.body;
+
+    const article = await articleService.createArticle(user_id, title, content);
+
+    res.status(200).json(article);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({});
+  }
+});
+
+// メモ更新
+router.put("/:id", async (req, res) => {
+  try {
+    // const user_id = req.user.user_id;
+    const user_id = 1; // 仮のユーザーID
+    const article_id = req.params.id;
+    const { title, content } = req.body;
+
+    const article = await articleService.updateArticle(
+      user_id,
+      article_id,
+      title,
+      content,
+    );
+
+    res.status(200).json(article);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+// メモ削除
+router.delete("/:id", async (req, res) => {
+  try {
+    // const user_id = req.user.user_id;
+    const user_id = 1; // 仮のユーザーID
+    const article_id = req.params.id;
+
+    await articleService.deleteArticle(user_id, article_id);
+
+    res.status(200).json({ message: "deleted" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "server error" });
   }
 });
 
--- backend/src/services/articles/ArticleService.js
+++ backend/src/services/articles/ArticleService.js
@@ -1,3 +1,7 @@
+import db from "../../models/index.js";
+
+const { Articles } = db;
+
 // クラス
 class ArticleService {
   /**
@@ -5,16 +9,71 @@ class ArticleService {
    * @param user_id
    * @return ランダム値
    */
-  getArticleList(user_id) {
-    return [{}];
+  async getArticleList(user_id) {
+    const articleList = await Articles.findAll({
+      where: {
+        author_id: user_id,
+      },
+      order: [["updated_at", "DESC"]],
+    });
+    return articleList;
   }
   /**
    * 記事情報取得
    * @param user_id
    * @return ランダム値
    */
-  getArticle(user_id, article_id) {
-    return {};
+  async getArticle(user_id, article_id) {
+    const article = await Articles.findOne({
+      where: {
+        id: article_id,
+        author_id: user_id,
+      },
+    });
+    return article;
+  }
+
+  // 記事新規作成
+  async createArticle(user_id, title, content) {
+    const article = await Articles.create({
+      title: title,
+      content: content,
+      author_id: user_id,
+    });
+    return article;
+  }
+
+  // 記事更新
+  async updateArticle(user_id, article_id, title, content) {
+    await Articles.update(
+      {
+        title: title,
+        content: content,
+      },
+      {
+        where: {
+          id: article_id,
+          author_id: user_id,
+        },
+      },
+    );
+
+    return await Articles.findOne({
+      where: {
+        id: article_id,
+        author_id: user_id,
+      },
+    });
+  }
+
+  // 記事削除
+  async deleteArticle(user_id, article_id) {
+    await Articles.destroy({
+      where: {
+        id: article_id,
+        author_id: user_id,
+      },
+    });
   }
 }
 
